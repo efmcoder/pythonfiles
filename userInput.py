881e33c37d4fea10dddd17cdb75b36ed26d5f1d7
efmcoder <faithefmcoder@gmail.com>
2018-08-08 22:50:45 -0400

    first commit

---
+++ userInput.py
@@ -0,0 +1,9 @@
+
+print("Enter your name:")
+x = input()
+print("Hello, " + x)
+
+#To run file, type in command LINE
+#python filename.py
+#Will prompt you for input, so enter your name
+#Result will load
