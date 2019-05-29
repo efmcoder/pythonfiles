53da2189be1981b74cbe3083c7f061fae8cfb73f
efmcoder <faithefmcoder@gmail.com>
2019-05-28 20:42:40 -0400

    python indented lines printing or not printing

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,16 @@
 print("Hello, World!")
 
+
+#Functions in python always start with the word "def"
 def sing():
     print("you may say I'm a dreamer \nbut I'm not the only one \nI hope some day you'll join us \nand the world will live as one")
 
 sing()
+#call the function sing. Note there are no colons or semicolons
+
+def loading_screen():
+    print("This page is loading...")
+    print("This will print because it's indented with above line")
+print("This will not print because it's not indented")
+
+loading_screen()
